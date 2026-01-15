f4d45428a244689d72c69b76b41e91ee804e9963
Soundwave(73173) <omaraslan2016@gmail.com>
2026-01-15 10:07:14 +0400

    Task 1 - initial Spring Boot application

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring/FirstProjectJavaSpring73173Application.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpring73173Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpring73173Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/first_project_java_spring/FirstProjectJavaSpring73173ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaSpring73173ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
